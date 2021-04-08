4906f643d67fd93bb952663d56cfc16847be98e9
ritabrata1995 <57553879+ritabrata1995@users.noreply.github.com>
2021-04-08 16:18:38 +0530

    node project part 1

---
+++ Sprint2/src/app.ts
@@ -0,0 +1,8 @@
+import express = require("express");
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+export default app;
