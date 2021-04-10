6cecf8dc3f14f92f3ead7436536231d4aebb00e2
ritabrata1995 <57553879+ritabrata1995@users.noreply.github.com>
2021-04-10 20:21:11 +0530

    Typescript arrow function

---
+++ Sprint2_3rd Assignment/main.js
@@ -0,0 +1,12 @@
+var sum = function (a, b) {
+    var num = 10;
+    if (a === b) {
+        return a + num;
+    }
+    else {
+        var a_1 = 10;
+        var b_1 = 20;
+        return a_1 + b_1;
+    }
+};
+console.log(sum(5, 5));
---
+++ Sprint2_3rd Assignment/main.ts
@@ -0,0 +1,11 @@
+let sum = (a: number, b:number) => {
+    const num = 10;
+    if( a === b){
+        return a + num;
+    }  else {
+        let a = 10;
+        let b = 20;
+        return a + b
+    }
+};
+console.log(sum(5,5));
